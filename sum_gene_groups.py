6f179f6fb20313df09a62cbe9ad5b9e6d578a67b
emily.josephs <emily.josephs@mustang.biol.mcgill.ca>
2015-03-18 09:02:20 -0400

    takes scaf separated files now

--- sum_gene_groups.py
+++ sum_gene_groups.py
@@ -2,7 +2,7 @@ import sys
 import numpy
 
 if len(sys.argv) < 3:
-	print("python sum_gen_groups.py [data file] [gene list]")
+	print("python sum_gen_groups.py [data file prefix] [gene list]")
 	sys.exit()
 
 # read in the gene list
@@ -13,22 +13,25 @@ for gene in genes:
 
 genes.close()
 modName = sys.argv[2].split('/')[-1].split('.')[0]
+numFields = None
 
-myData = open(sys.argv[1],'r')
-header = myData.readline() #skip header
-outData = []
-numFields = len(header.split())-1
-for i in range(0,numFields):
-	outData.append([])
+for i in range(1,9):
+	myData = open(sys.argv[1]+str(i),'r')
+	header = myData.readline() #skip header
+	if not numFields: #if first file, figure out the number of fields 
+		numFields = len(header.split())-1
+		outData = []
+		for i in range(0,numFields):
+			outData.append([]) #,ale pitData
+	for line in myData:
+		if line[0:4] == "gene":
+			continue #header gile
+		gene, data = line.split()[0], [int(x) for x in line.split()[1:]]
+		if gene in geneList:
+			#outData = [x+y for x,y in zip(outData,data)]
+			for i in range(0, numFields):
+				outData[i].append(data[i])
 
-for line in myData:
-	if line[0:4] == "gene":
-		continue #header gile
-	gene, data = line.split()[0], [int(x) for x in line.split()[1:]]
-	if gene in geneList:
-		#outData = [x+y for x,y in zip(outData,data)]
-		for i in range(0, numFields):
-			outData[i].append(data[i])
 means = [str(float(sum(x))/len(x)) for x in outData]
 print(modName+"	means	"+"	".join(means))
 errs =  [str(numpy.std(numpy.array(x))) for x in outData]
